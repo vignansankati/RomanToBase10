ad2ee5ba45f7694981ecc4faee13e3d6df9b7606
Vignan Sankati <vignan.nani@gmail.com>
2017-04-19 20:36:57 -0400

    Initial Commit

---
+++ RomanToBase10Tests/RomanToBase10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RomanToBase10Tests.swift
+//  RomanToBase10Tests
+//
+//  Created by Vignan Sankati on 4/19/17.
+//  Copyright © 2017 Vignan Sankati. All rights reserved.
+//
+
+import XCTest
+@testable import RomanToBase10
+
+class RomanToBase10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
